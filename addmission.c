c670159fb257a6c9b875de238485d309b4d44099
Nahid-mahmud555 <162360388+Nahid-mahmud555@users.noreply.github.com>
2024-04-17 11:47:14 +0600

    Create addmission.c

---
+++ addmission.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float result;
+printf("Wellcome to our Collage.\n For your addmission you should must fill up some rules.Than we take dessicion.\n");
+printf("Enter your ssc  result =  ");
+scanf("%f",&result);
+if(result>=4.00){
+    printf("wellcome to our collage \n ");
+}
+else{
+    printf("Sorry.your result is too low for addmission in our collage");
+}
+
+    return 0;
+}
